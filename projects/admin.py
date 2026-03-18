41a8efb30ae7b31d08f231b347a4ae2673a90b25
Nkosinathi <nkosimkhonza5@gmail.com>
2026-03-18 09:55:44 +0200

    feat: register Project model with Django admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display  = ['id', 'title', 'tech_stack', 'status', 'created_at']
+    list_filter   = ['status']
+    search_fields = ['title', 'tech_stack']
+    ordering      = ['-created_at']
